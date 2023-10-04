228ece29cdbfb51c5668519710ec7c89a673fdbc
michaelfigelman <mfigelman@gmail.com>
2023-10-04 13:32:21 -0600

    started combining db code with spotify api
    combined all code written so far to test retrieving the currently playing song and store song info in a db so that it can be retrieved from an outside GET request.
    
    Next I will modularize this in a more efficient way and retest.

--- index.py
+++ index.py
@@ -9,55 +9,56 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 db = SQLAlchemy(app)
 
 class SongModel(db.Model):
-    id = db.Column(db.Integer, primary_key = True)
-    name = db.Column(db.String(100), nullable=False)
-    artist = db.Column(db.String(100), nullable=False)
-    album = db.Column(db.String(100), nullable=False)
+    id = db.Column(db.String, primary_key = True)
+    song_name = db.Column(db.String(100), nullable=False)
+    artist_name = db.Column(db.String(100), nullable=False)
+    album_name = db.Column(db.String(100), nullable=False)
     length = db.Column(db.Integer, nullable=False)
 
+
     def __repr__(self):
-        return f'Song(name={name}, artist={artist}, album = {album}, length = {length} seconds)'
-
-# db.create_all()
+        return f'Song(name={self.song_name}, artist={self.artist_name}, album = {self.album_name}, length = {self.length} milliseconds)'
 
 
-video_put_args = reqparse.RequestParser()
-video_put_args.add_argument("name", type = str, help="name of song")
-video_put_args.add_argument("artist", type = str, help="artist of song")
-video_put_args.add_argument("album", type = str, help="album of song")
-video_put_args.add_argument("length", type = int, help="length of song")
+song_put_args = reqparse.RequestParser()
+song_put_args.add_argument("id", type=str, help="song id")
+song_put_args.add_argument("song_name", type = str, help="name of song")
+song_put_args.add_argument("artist_name", type = str, help="artist of song")
+song_put_args.add_argument("album_name", type = str, help="album of song")
+song_put_args.add_argument("length", type = int, help="length of song")
 
 resource_fields = {
-    'id': fields.Integer,
-    'name': fields.String,
-    'artist': fields.String,
-    'album': fields.String,
+    'id': fields.String,
+    'song_name': fields.String,
+    'artist_name': fields.String,
+    'album_name': fields.String,
     'length': fields.Integer
 }
-
-class song(Resource):
+class get_song(Resource):
 
     @marshal_with(resource_fields)
-    def get(self, song_id):
-        result = SongModel.query.get(id=song_id)
+    def get(self, id):
+        result = SongModel.query.get(id=id)
         return result
 
+class add_song(Resource):
+
     @marshal_with(resource_fields)
-    def put(self, song_id):
-        args = video_put_args.parse_args()
-        song = SongModel(id=song_id, name=args['name'], artist=args['artist'], album=args['album'], length=args['length'])
+    def put(self, id):
+        args = song_put_args.parse_args()
+        song = SongModel(id=args['id'], song_name=args['song_name'], artist_name=args['artist_name'], album_name=args['album_name'], length=args['length'])
         db.session.add(song)
         db.session.commit()
         return song, 201
     
-    def delete(self, song_id):
-        abort_if_song_not_in_songs(song_id)
-        del songs[song_id]
-        return '', 204
 
-    
+api.add_resource(get_song, "/song/get/<string:id>")
+api.add_resource(add_song, "/song/add/<string:id>")
 
-api.add_resource(song, "/song/<int:song_id>")
+def handle_error(e):
+    return jsonify(error=str(e)), 500
 
 if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
--- test.py
+++ test.py
@@ -1,55 +1,21 @@
 import requests
 import json
 
-BASE = "http://127.0.0.1:5000/"
-data = [{
-        "name": "Rhiannon", 
-        "artist":"Fleetwood Mac", 
-        "album":"Fleetwood Mac",
+track_info = {
+        "id": "1239",
+        "song_name": "The Chain", 
+        "artist_name":"Fleetwood Mac", 
+        "album_name":"Fleetwood Mac",
         "length": 252
-    },
-    {
-        "name": "Is this it", 
-        "artist":"The Strokes", 
-        "album":"Is this it",
-        "length": 180
-    },
-    {
-        "name": "Black Dog", 
-        "artist":"Led Zeppelin", 
-        "album":"Led Zeppelin IV",
-        "length": 295
-    },
-    {
-        "name": "Tumbling Dice", 
-        "artist":"The Rolling Stones", 
-        "album":"Exile On Main Street",
-        "length": 226
-    }]
+    }
+
+BASE = "http://127.0.0.1:5000/"
+link = f"song/add/{track_info['id']}"
 
 headers = {'Content-Type': 'application/json'}
-i=0
+track_info_json = json.dumps(track_info)
 
-while i < len(data):
-    data_json = json.dumps(data[i])
-    link = (f'song/{i}')
-    put_response = requests.put(BASE + link, data=data_json, headers=headers)
-    i+=1
+put_response = requests.put(BASE + link, data=track_info_json, headers=headers)
+print(put_response.status_code)
 
-j = 0
-while j < len(data):
-    link = (f'song/{j}')
-    get_response = requests.get(BASE + link)
-    print(get_response.json())
-    j+=1
 
-song_id_to_delete = 'song/2'
-del_response = requests.delete(BASE + song_id_to_delete)
-print(del_response)
-
-j = 0
-while j < len(data):
-    link = (f'song/{j}')
-    get_response = requests.get(BASE + link)
-    print(get_response.json())
-    j+=1
