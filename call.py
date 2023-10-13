71252e61acffd444ac66c522fd97e0cf72ffbfce
michaelfigelman <mfigelman@gmail.com>
2023-10-13 15:51:24 -0600

    Updated GetTrackInfo and GetCurrentlyPlaying to use AudioAnalysis model
    The flask server now stores all the audio features of the currently playing song to the AudioAnalysis table
    
    the song/currentlyplaying/ endpoint now returns the AudioAnalysis table which contains all necessary info

--- call.py
+++ call.py
@@ -3,12 +3,13 @@ import pandas as pd
 import json
 
 base = 'http://127.0.0.1:5000/'
-link = f"/song/recent/"
+link = f"/song/currentlyplaying/"
 headers = {}
 
 get_request = requests.get(base+link)
 response_json_list = get_request.json()
+print(response_json_list)
 
-response_json = json.dumps(get_request.json())
-df = pd.read_json(response_json)
-print(df.columns)
+# response_json = json.dumps(get_request.json())
+# df = pd.read_json(response_json)
+# print(df.columns)
